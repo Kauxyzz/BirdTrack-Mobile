744a84807a3f9ee137335d6ae6e3adb6713ef3ac
Kauxyzz <Khfmunchen@minha.fag.edu.br>
2025-06-18 20:20:43 -0300

    Versao.0.5

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,5 @@
-import { Stack } from 'expo-router';
+import { Slot } from "expo-router";
 
 export default function Layout() {
-  return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: 'Dashboard' }} />
-      <Stack.Screen name="login" options={{ title: 'Login', presentation: 'modal' }} />
-      <Stack.Screen name="documentos" options={{ title: 'Documentos' }} />
-      <Stack.Screen name="monitoramento" options={{ title: 'Monitoramento de Produção' }} />
-    </Stack>
-  );
+  return <Slot />;
 }
